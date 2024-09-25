a9934b5441a8c022e5cd3340a9f6b0a3d0c469ab
ankitsharma06 <asrover9810@gmail.com>
2024-09-25 19:08:21 +0530

    cors updated changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://job-hunt-frontend-three.vercel.app/'], // Allow both localhost and Vercel
+    origin: ['http://localhost:5173', 'https://job-hunt-frontend-omega.vercel.app'], // Allow both localhost and Vercel
     credentials: true
 };
 
